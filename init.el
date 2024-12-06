ac90a81c73084991870999b1903f4dfe6c08eeae
Taz <tsyroid@gmail.com>
2024-12-05 21:05:15 -0800

    Reset to start of V2

--- init.el
+++ init.el
@@ -1,15 +1,26 @@
 ;; 2024-12-04 Zamansky LESSON1
 ;; ~/.emacs.d/init.el
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(which-key try)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (setq inhibit-startup-message t)
 
 
 (require 'package)
 (setq package-enable-at-startup nil)
-
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-
 (package-initialize)
 
 ;; Bootstrap `use-package'
