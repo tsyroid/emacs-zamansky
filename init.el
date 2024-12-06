b8350783f709dffab509b3cbfbd95c59f4bf7d78
Taz <tsyroid@gmail.com>
2024-12-06 00:28:43 -0800

    LESSON-7: navigating with avy

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-;; 2024-12-04 Zamansky LESSON-X
 ;; ~/.emacs.d/init.el
 
 ;; START MYSTUFF
@@ -8,80 +7,94 @@
 (setq make-backup-files nil)
 ;; END MYSTUFF
 
+
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
-        (package-refresh-contents)
-        (package-install 'use-package))
+	(package-refresh-contents)
+	(package-install 'use-package))
 
 (use-package try
-  :ensure t)
+	:ensure t)
 
 (use-package which-key
-  :ensure t
-  :config
-  (which-key-mode))
+	:ensure t 
+	:config
+	(which-key-mode))
 
-;; Org mode stuff
+
+;; Org-mode stuff
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-;; Ido-mode
-(setq indo-enable-flex-matching t)
-(setq ido-everywhere t)
-;; (ido-mode 1)
+
+
+;; using swiper so ido no longer needed
+;;(setq ido-enable-flex-matching t)
+;;(setq ido-everywhere t)
+;;(ido-mode 1)
 
 (defalias 'list-buffers 'ibuffer)
 ;; (defalias 'list-buffers 'ibuffer-other-window)
 
-;; ;; If you're interested in tab functionality
-;; (use-package tabbar
-;;   :ensure t
-;;   :config
-;;   (tabbar-mode 1))
+; If you like a tabbar 
+;(use-package tabbar
+;  :ensure t
+;  :config
+;  (tabbar-mode 1))
+
 
 (use-package ace-window
   :ensure t
   :init
   (progn
     (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
     ))
+
 
 ;; it looks like counsel is a requirement for swiper
 (use-package counsel
-:ensure t
-)
+  :ensure t
+  )
+
+(use-package ivy
+  :ensure t
+  :diminish (ivy-mode)
+  :bind (("C-x b" . ivy-switch-buffer))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-display-style 'fancy))
+
 
 (use-package swiper
   :ensure try
+  :bind (("C-s" . swiper)
+	 ("C-r" . swiper)
+	 ("C-c C-r" . ivy-resume)
+	 ("M-x" . counsel-M-x)
+	 ("C-x C-f" . counsel-find-file))
   :config
   (progn
-  (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
-  (global-set-key "\C-s" 'swiper)
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  (global-set-key (kbd "<f6>") 'ivy-resume)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-  (global-set-key (kbd "<f1> l") 'counsel-load-library)
-  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c k") 'counsel-ag)
-  (global-set-key (kbd "C-x l") 'counsel-locate)
-  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
-  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-))
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq ivy-display-style 'fancy)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+    ))
+
+(use-package avy
+  :ensure t
+  :bind ("M-s" . avy-goto-char))
