087d02b7b1e69db7f4ae6c199a22f37b174f98c7
Taz <tsyroid@gmail.com>
2024-12-06 00:05:06 -0800

    LESSON-6: search + swiper

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ace-window which-key try org-bullets)))
+ '(package-selected-packages '(counsel ace-window which-key try org-bullets)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
